23922f79137c288ecbfeff56f6cbaaa22b69177a
venkaiah.koneru <P@ssw0rd>
2018-08-05 11:57:52 +0400

    Use SNS for fan-out messaging

--- stream/src/index.ts
+++ stream/src/index.ts
@@ -1,11 +1,12 @@
-import { S3, SQS } from 'aws-sdk'
+import { S3, SNS } from 'aws-sdk'
 import { v4 } from 'uuid'
 import { createReadStream } from 'fs'
 import { sample, random } from 'lodash'
 import { join } from 'path'
+import { createClient } from 'redis';
 
 const s3 = new S3({ endpoint: 'http://localhost:5000', s3ForcePathStyle: true })
-const sqs = new SQS({ endpoint: 'http://localhost:5001' })
+const sns = new SNS({ endpoint: 'http://localhost:5002'})
 
 const uploadFile = async (filePath: string, Bucket: string, Key: string = filePath) => {
   return s3
@@ -18,34 +19,40 @@ const uploadFile = async (filePath: string, Bucket: string, Key: string = filePa
     .promise()
 }
 
-const createDocument = async (QueueUrl: string, Bucket: string) => {
+const createDocument = async (TopicArn: string, Bucket: string) => {
   const samples = ['drylab.pdf', 'example.pdf', 'flyer.pdf', 'magic.pdf', 'PrinceCatalogue.pdf']
   const filePath = join(__dirname, `documents/${sample(samples)}`)
   const uid = v4()
   const Key = `${uid}.pdf`
   await uploadFile(filePath, Bucket, Key)
-  await sqs.sendMessage({ QueueUrl, MessageBody: JSON.stringify({ uid, Bucket, Key }) }).promise()
+  await sns.publish({TopicArn, Message:  JSON.stringify({ uid, Bucket, Key }) }).promise()
 }
 
 async function initialize() {
-  const QueueName = 'file-stream'
   const Bucket = 'file-stream'
-  const { QueueUrl } = await sqs.createQueue({ QueueName }).promise()
+  const Name = 'file-stream'
+  
+  const { TopicArn } = await sns.createTopic({Name}).promise();
+
+  const redis = await createClient(6379, 'localhost');
+  redis.set('topicArn', TopicArn);
+
+  // const { QueueUrl } = await sqs.createQueue({ QueueName }).promise()
   await s3.createBucket({ Bucket }).promise()
-  return { QueueUrl, Bucket }
+  return { TopicArn, Bucket }
 }
 
 async function generate(options) {
-  const { Bucket, QueueUrl } = options
+  const { Bucket, TopicArn } = options
   const timeout = random(1000)
   const interval = setTimeout(async () => {
-    await createDocument(QueueUrl, Bucket)
-    await createDocument(QueueUrl, Bucket)
-    await createDocument(QueueUrl, Bucket)
+    await createDocument(TopicArn, Bucket)
+    await createDocument(TopicArn, Bucket)
+    await createDocument(TopicArn, Bucket)
     console.log('Created 3 documents after ', timeout)
     clearTimeout(interval)
     generate(options)
-  }, timeout)
+  }, 10000)
 }
 
 initialize().then(generate)
