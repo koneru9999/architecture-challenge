17400dbe41bd9e38b8fa6b2f75c8ccbb54a11adf
venkaiah.koneru <P@ssw0rd>
2018-08-05 12:00:37 +0400

    handle queues internally and subscribe queues to with SNS

--- services/extract-images/src/index.ts
+++ services/extract-images/src/index.ts
@@ -1,4 +1,51 @@
 import { SQSLongPolling } from './sqs-long-polling-consumer';
+import { SQS, SNS } from 'aws-sdk';
+import { createClient } from 'redis';
+
+async function initialize() {
+  const QueueName = 'file-stream-images';
+  let sqs = new SQS({ endpoint: 'http://localhost:5001' });
+  const { QueueUrl } = await sqs.createQueue({ QueueName }).promise()
+
+  // Get Queue ARN
+  const { Attributes } = await sqs.getQueueAttributes({QueueUrl, AttributeNames: ["QueueArn"]}).promise()
+
+  // Get Topic ARN -- ideally this is acheived using AWS Console
+  const redisClient = createClient(6379, 'localhost');
+  redisClient.get('topicArn', async (err, topicArn) => {
+    await new SNS({endpoint: 'http://localhost:5002' }).subscribe({
+      Protocol: 'sqs',
+      TopicArn: topicArn,
+      Endpoint: Attributes.QueueArn
+    }).promise()
+  });
+}
+
+initialize().then(
+  () => {
+    // Simple configuration:
+    //  - 2 concurrency listeners
+    //  - each listener can receive up to 4 messages
+    // With this configuration you could receive and parse 8 `message` events in parallel
+    const queue = new SQSLongPolling({
+      name: 'file-stream-images',
+      maxNumberOfMessages: 4,
+      concurrency: 2
+    });
+
+    queue.on('message', e => {
+      console.log('New message from file-stream-images: ', e.data);
+      e.deleteMessage().then(() => {
+        e.next();
+      });
+    });
+
+    queue.on('error', err => {
+      console.log('There was an error: ', err);
+    });
+  }
+);
+
 
 // Simple configuration:
 //  - 2 concurrency listeners
--- services/extract-text/src/index.ts
+++ services/extract-text/src/index.ts
@@ -1,22 +1,47 @@
 import { SQSLongPolling } from './sqs-long-polling-consumer';
+import { SQS, SNS } from 'aws-sdk';
+import { createClient } from 'redis';
 
-// Simple configuration:
-//  - 2 concurrency listeners
-//  - each listener can receive up to 4 messages
-// With this configuration you could receive and parse 8 `message` events in parallel
-const queue = new SQSLongPolling({
-  name: 'file-stream',
-  maxNumberOfMessages: 4,
-  concurrency: 2
-});
+async function initialize() {
+  const QueueName = 'file-stream-text';
+  let sqs = new SQS({ endpoint: 'http://localhost:5001' });
+  const { QueueUrl } = await sqs.createQueue({ QueueName }).promise()
 
-queue.on('message', e => {
-  console.log('New message: ', e.data);
-  e.deleteMessage().then(() => {
-    e.next();
+  // Get Queue ARN
+  const { Attributes } = await sqs.getQueueAttributes({QueueUrl, AttributeNames: ["QueueArn"]}).promise()
+
+  // Get Topic ARN -- ideally this is acheived using AWS Console
+  const redisClient = createClient(6379, 'localhost');
+  redisClient.get('topicArn', async (err, topicArn) => {
+    await new SNS({endpoint: 'http://localhost:5002' }).subscribe({
+      Protocol: 'sqs',
+      TopicArn: topicArn,
+      Endpoint: Attributes.QueueArn
+    }).promise()
   });
-});
+}
 
-queue.on('error', err => {
-  console.log('There was an error: ', err);
-});
+initialize().then(
+  () => {
+    // Simple configuration:
+    //  - 2 concurrency listeners
+    //  - each listener can receive up to 4 messages
+    // With this configuration you could receive and parse 8 `message` events in parallel
+    const queue = new SQSLongPolling({
+      name: 'file-stream-text',
+      maxNumberOfMessages: 4,
+      concurrency: 2
+    });
+
+    queue.on('message', e => {
+      console.log('New message from file-stream-text: ', e.data);
+      e.deleteMessage().then(() => {
+        e.next();
+      });
+    });
+
+    queue.on('error', err => {
+      console.log('There was an error: ', err);
+    });
+  }
+);
